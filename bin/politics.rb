a276d2a33f046f2d99fe53cbf989aa0afe64b89f
Sean <sjgallen@gmail.com>
2016-03-07 10:24:29 -0600

    Working acceptance test

--- bin/politics.rb
+++ bin/politics.rb
@@ -2,23 +2,23 @@ def candidate_choice
   puts "1. Donald Trump\n2. Ben Carson\n3. Marco Rubio\n4. Hillary Clinton\n5. Chris Christie"
   character = gets.chomp
   case character
-  when 1
+  when "1"
     donald
-  when 2
+  when "2"
     ben
-  when 3
+  when "3"
     marco
-  when 4 
+  when "4"
     hillary
-  when 5
+  when "5"
     chris
   else
     "Please enter a valid choice."
   end
 end
 
-  #unsure if you can call a method from within a case. I can't get it to work or find documentation on it.
-  #
+#unsure if you can call a method from within a case. I can't get it to work or find documentation on it.
+#
 def donald
   puts "You selected: The Donald! He's Yuuuuuuuuuge."
   puts "The Donald starts out with:\n100/100 moneys\n60/100 health\n85/100 charisma"  end
@@ -29,14 +29,16 @@ puts "\nGet ready to show the public what you stand for..."
 sleep 3
 puts "...and what you will fall for!"
 sleep 2
-
+system "clear"
 puts "Press 'h' for help, and 'q' to quit. Press any other key to continue."
 user_input = $stdin.gets.chomp.to_s
 if user_input == "q"
-  puts "You've died because you suck"
+  # puts "You've died because you suck"
+elsif user_input == "h"
+  # puts "Welcome to the help screen."
 else
-  puts "Who will you be campaigning as?"
-  candidate_choice
+  # puts "Who will you be campaigning as?"
+  # candidate_choice
 end
 
 
---
+++ spec/acceptance_test.rb
@@ -0,0 +1,65 @@
+require 'io/console'
+require 'politics'
+
+class GameTest < Minitest::Test
+  def test_displays_all_information_and_allows_you_to_quit
+    clear = "\e[H\e[2J"
+    game = Politics::Cli.new # <-- this thing can know about the command line, push the logic below it
+
+    # if the string assertions get annoying, switch this to match against regexes
+    # (assert_match and a regex instead of assert_equal and a string)
+    to_print = game.input("")
+    assert_equal "Welcome to the Presidential Raucuses!", to_print
+
+    to_print = game.input("\n")
+    assert_equal "...and what you will fall for!", to_print
+
+    to_print = game.input("\n")
+    assert_equal "#{clear}After a hard fought 6 months, you find yourself even in the public opinion polls.\nHow can the public not see what you are saying is the only way to proceed?\nThey must be morons.\n\n"+
+                 "It's the beginning of debate season, and a time of great change. Not only do you have the chance to make America great again, but we have the chance to change the debate schedule and make arguing great again.\n\n"+
+                 "There is only one debate this year, and it is between all of the candidates. This is your shot, America will be yours again...\nI mean great, again.\n\n"+
+                 "The moderator takes her seat...\n"+
+                 "The first question looms...\n"+
+                 "The moderator laughs.\n",
+                 to_print
+
+    to_print = game.input("\n")
+    assert_equal "Moderator: Mr. Trump, our first question comes from George in Ohio.\n"+
+                 "George wants to know, who would win in a cagefight?\n"+
+                 "You or Hillary Clinton?\n",
+                 to_print
+
+    to_print = game.input("\n")
+    assert_equal "The audience chuckles. This is your shot.\n"+
+                 "You tell the audience that you don't need to answer this question.\n"+
+                 "You slick your hair into it's signature swirl and slink over to Hillary's podium.\n",
+                 to_print
+
+    to_print = game.input("\n")
+    assert_equal "As you saunter to just a mere inchess before her,  She begind to spin. First slowly, then quickly building to a whirlwind.\n"+
+                 "The conservative blue pantsuit she was wearing flashes rapidly and morphs into a patriotic mess of sparkles and gold lame'.\n",
+                 to_print
+
+    to_print = game.input("\n")
+    assert_equal "The expression on her face has turned from stoic, to maniacal.\n"+
+                 "Her eyes shine with the fire of burned aspirations and hopes, a determination to excell at all costs.\n",
+                 to_print
+
+    to_print = game.input("\n")
+    assert_equal "You know what you have to do\n", to_print
+
+    # maybe more talking?
+
+    to_print = game.input("\n")
+    assert_equal "Do you:\n" +
+                 "1. Pull the sharpened pen you stole from the bank from your hair?\n" +
+                 "2. Have a heart attack.\n",
+                 to_print
+
+    refute game.over?
+    to_print = game.input("2\n")
+    assert game.over?
+    assert_equal "You died of a heart attack.\nThe end.\n", to_print
+  end
+end
+
